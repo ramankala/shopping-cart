b37031884be5da9865b18ccbbcdf503c15583502
Raman <rs.kala@hotmail.com>
2022-07-27 03:52:59 -0400

    .

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteSwitch from './RouteSwitch';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <HashRouter>
-        <RouteSwitch />
-    </HashRouter>
+    <RouteSwitch />
 );
